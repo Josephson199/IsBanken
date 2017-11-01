09575d6377790c3f2cf55bbd157362262d546fb3
mattias josephsson <josephson199@gmail.com>
2017-11-01 14:26:26 +0100

    Add domain models -Customer -Transaction -Account

---
+++ IsBanken.Buisness/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IsBanken.Buisness.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string OrganizationId { get; set; }
+        public string CompanyName { get; set; }
+        public string SreetAddress { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+        public string Phonenumber { get; set; }
+        public List<Account> Accounts { get; set; }
+    }
+}
