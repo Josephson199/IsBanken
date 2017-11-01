04d46472964f0ed1efb73e0d0b87051d16e32580
mattias josephsson <josephson199@gmail.com>
2017-11-01 15:33:00 +0100

    Add fileHandler

--- IsBanken.Buisness/Models/Account.cs
+++ IsBanken.Buisness/Models/Account.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace IsBanken.Buisness.Models
@@ -7,7 +8,8 @@ namespace IsBanken.Buisness.Models
     public class Account
     {
         public int AccountId { get; set; }
-        public int CustomerId { get; set; }
-        public decimal Balance { get; set; }
+        public int CustomerId { get; set; }        
+        public List<Transaction> Transactions { get; set; }
+        public decimal Balance { get { return Transactions.Sum(t => t.Amount) + Balance; }  }
     }
 }
