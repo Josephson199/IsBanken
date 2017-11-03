02810263e4e98ece978119f2789cc10ef2f39d9a
PatricStromberg <stromberg.patric@hotmail.com>
2017-11-03 16:29:35 +0100

    Implement UI (#8)
    Looks good!

--- IsBanken.ConsoleApp/Program.cs
+++ IsBanken.ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using IsBanken.Buisness.Infrastructure;
 using System;
+using System.Globalization;
 
 namespace IsBanken.ConsoleApp
 {
@@ -25,14 +26,145 @@ namespace IsBanken.ConsoleApp
 
         static void Main(string[] args)
         {
-            Initialize();            
+            Initialize();
 
-            Console.WriteLine(_bank.GetCustomers().Count);
-            Console.WriteLine(_bank.GetAccounts().Count);
-            
-            Console.ReadLine();
+
+            Console.WriteLine("\n>>>>>>>>>>Välkommen till IsBanken<<<<<<<<<<\n\n");
+            Console.WriteLine("Antal kunder: " + _bank.GetCustomers().Count);
+            Console.WriteLine("Antal konton: " + _bank.GetAccounts().Count);
+            //todo saldo
+            Console.WriteLine("Totalt saldo:");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            DisplayMenu();
+            Console.Write("> ");
+            var run = true;
+            while (run)
+            {
+                var input = Console.ReadLine();
+                if (input.ToLower().Trim() == "hjälp")
+                {
+                    DisplayMenu();
+                    Console.WriteLine();
+                    continue;
+                }
+                var inputParsedResult = int.TryParse(input, out int result);
+                if (inputParsedResult)
+                {
+                    switch (result)
+                    {
+                        case 1:
+                            SearchCustomer();
+                            break;
+                        case 2:
+                            ShowCustomer();
+                            break;
+                        case 3:
+                            CreateCustomer();
+                            break;
+                        case 4:
+                            DeleteCustomer();
+                            break;
+                        case 5:
+                            CreateAccount();
+                            break;
+                        case 6:
+                            DeleteAccount();
+                            break;
+                        case 7:
+                            Deposit();
+                            break;
+                        case 8:
+                            Withdraw();
+                            break;
+                        case 9:
+                            AddTransaction();
+                            break;
+                        case 0:
+                            SaveFile();
+                            run = false;
+                            break;
+                        default:
+                            Console.WriteLine();
+                            break;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Ange ett nummer mellan 0-9");
+                }
+            }
+
+
         }
 
-       
+        private static void SaveFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void AddTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Withdraw()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Deposit()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void DeleteAccount()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void CreateAccount()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void DeleteCustomer()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void CreateCustomer()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ShowCustomer()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void SearchCustomer()
+        {
+            throw new NotImplementedException();
+        }
+
+        static void DisplayMenu()
+        {
+            Console.WriteLine("HUVUDMENY");
+            Console.WriteLine();
+            Console.WriteLine("0. Avsluta och spara");
+            Console.WriteLine("1. Sök kund");
+            Console.WriteLine("2. Visa kundbild");
+            Console.WriteLine("3. Skapa kund");
+            Console.WriteLine("4. Ta bort kund");
+            Console.WriteLine("5. Skapa konto");
+            Console.WriteLine("6. Ta bort konto");
+            Console.WriteLine("7. Insättning");
+            Console.WriteLine("8. Uttag");
+            Console.WriteLine("9. Överföring");
+            Console.WriteLine();
+        }
     }
 }
