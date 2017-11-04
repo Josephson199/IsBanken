049a16be14e01f7a54edbb9d9097806fe4d1da0f
mattias josephsson <josephson199@gmail.com>
2017-11-04 15:23:44 +0100

    Fixed pathing for filehandler.

--- IsBanken.Buisness/Infrastructure/FileHandler.cs
+++ IsBanken.Buisness/Infrastructure/FileHandler.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using System.IO;
 using IsBanken.Buisness.Models;
+using System;
+using System.Linq;
+using System.Globalization;
 
 namespace IsBanken.Buisness.Infrastructure
 {
     public class FileHandler : IFileHandler
     {
-        private readonly string path =
-            @"C:\Users\patricstromberg\source\repos\IsBanken\IsBanken.Buisness\Files\bankdata.txt";
+        private readonly string _path;
+        private readonly string _bankDataStorageFolder = @"..\IsBanken.Buisness\Files\BankDataStorage\";
+        private readonly string _dateFormat = "yyyy-MM-dd_hh-mm-ss.fff";
+
+        public FileHandler()
+        {
+            _path = Directory.GetFiles(_bankDataStorageFolder, "*.txt")
+                                    .Select(Path.GetFileNameWithoutExtension)
+                                    .Select(f => DateTime.ParseExact(f, _dateFormat, CultureInfo.GetCultureInfo("sv-SE")))
+                                    .OrderByDescending(d => d)
+                                    .FirstOrDefault()
+                                    .ToString(_dateFormat) + ".txt";
+        }
+
         public List<string> ReadFile()
         {
             var fileLines = new List<string>();
-            using (var sr = new StreamReader(path))
+            using (var sr = new StreamReader(_bankDataStorageFolder + _path))
             {
                 while (sr.ReadLine() != null)
                 {
@@ -25,7 +40,7 @@ namespace IsBanken.Buisness.Infrastructure
 
         public void SaveFile(List<Account> accounts, List<Customer> customers)
         {
-            throw new System.NotImplementedException();
+            var savePath = $"{_bankDataStorageFolder}{DateTime.Now.ToString(_dateFormat, CultureInfo.GetCultureInfo("sv-SE"))}.txt";
         }
     }
 }
